a5baa55a0898e87e34a0c4d4b6418a4aaf58729e
Jill VandenDriessche <jill.vandendriessche@howest.be>
2024-03-14 13:48:03 +0100

    Changed url for API server

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,11 +1,12 @@
 const GROUPNUMBER = "00";
+const LANG = "nl";
 const GAMEPREFIX = "group00";
 
 const ERRORHANDLERSELECTOR = ".errormessages p";
 
 const LOCALSERVER = `http://localhost:8080`;
-const DEPLOYEDSERVER = `https://project-1.ti.howest.be/2023-2024/group-00/api`;
-const GROUPDEPLOYEDSERVER = `https://project-1.ti.howest.be/2023-2024/group-${GROUPNUMBER}/api`;
+const DEPLOYEDSERVER = `https://project-1.ti.howest.be/2023-2024/labyrinth/api/`;
+const GROUPDEPLOYEDSERVER = `https://project-1.ti.howest.be/2023-2024/${LANG}/group-${GROUPNUMBER}/api`;
 
 function getAPIUrl() {
   return DEPLOYEDSERVER;
