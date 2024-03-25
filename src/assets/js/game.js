981425c06ed9984de05ad26b02cad86e287dbbaf
grump <happyalex24563@gmail.com>
2024-03-25 16:21:49 +0100

    generated Board

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -3,9 +3,24 @@ init();
 
 
 function init() {
+    generateBoard();
     createEventListeners();
 }
 
+function generateBoard() {
+    const board = document.querySelector('#board');
+    for (let columns = 0; columns < 8; columns++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        board.appendChild(column);
+        for (let rows = 0; rows < 8; rows++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.setAttribute('data-target', `${columns}, ${rows}`);
+            column.appendChild(square);
+        }
+    }
+}
 
 
 //TODO: update button event listener with navigate function from universal.js
