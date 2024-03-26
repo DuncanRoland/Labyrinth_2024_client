aea210a0a14298c690aae69da8672d47e9f82450
grump <happyalex24563@gmail.com>
2024-03-26 09:36:59 +0100

    Created randomly generated treasure objectives

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -1,10 +1,12 @@
-
+import * as CommunicationAbstractor from "./data-connector/api-communication-abstractor.js";
+import * as ErrorHandler from "./data-connector/error-handler.js";
 init();
 
 
 function init() {
     generateBoard();
     createEventListeners();
+    createTreasureObjectives();
 }
 
 function generateBoard() {
@@ -16,7 +18,7 @@ function generateBoard() {
         for (let rows = 0; rows < 8; rows++) {
             const square = document.createElement('div');
             square.classList.add('square');
-            square.setAttribute('data-target', `${columns}, ${rows}`);
+            square.setAttribute('data-target', `${columns},${rows}`);
             column.appendChild(square);
         }
     }
@@ -34,4 +36,20 @@ function createEventListeners() {
 function getBoardPiece(e) {
     e.preventDefault();
     console.log(e.target);
+}
+
+async function createTreasureObjectives(maxObjectives = 3) {
+    const treasures = await CommunicationAbstractor.fetchFromServer('/treasures', 'GET').catch(ErrorHandler.handleError);
+    const objectives = [];
+    for (let i = 0; i < maxObjectives; i++) {
+        const randomTreasure = treasures.treasures[Math.floor(Math.random() * treasures.treasures.length)];
+        objectives.push(randomTreasure);
+    }
+    objectives.forEach(objective => { createDiv('li', objective, document.querySelector(`#treasureList`)) });
+}
+
+function createDiv(elementName, inner, container) {
+    const element = document.createElement(elementName);
+    element.innerHTML = inner;
+    container.appendChild(element);
 }
