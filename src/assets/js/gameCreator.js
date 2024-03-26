a7f00ce0a5f1faf06809ac1ffd6bf0a49680fe17
Zjefvm <66432723+Zjefvm@users.noreply.github.com>
2024-03-26 11:33:02 +0100

    Reformatted the js file

--- src/assets/js/gameCreator.js
+++ src/assets/js/gameCreator.js
@@ -3,14 +3,12 @@ import {navigate} from './universal.js';
 function init() {
     const backButton = document.querySelector('#back');
     backButton.addEventListener('click', () => {
-            console.log("Clicked on the button with destination index.html");
             navigate('index.html');
         }
     );
 
     const createGameButton = document.querySelector('#createGame');
     createGameButton.addEventListener('click', () => {
-            console.log("Clicked on the button with destination gameCreated.html");
             navigate('gameCreated.html');
         }
     );
