56a9a93ef4e188eeff84a98b25eb2c12095308fe
Duncan <r0897472@student.vives.be>
2024-03-27 10:14:55 +0100

    added some js

---
+++ src/assets/js/gameList.js
@@ -0,0 +1,19 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", initGameListPage);
+
+function initGameListPage() {
+    findGameId();
+    joinGame();
+
+}
+
+function findGameId(event) {
+    event.preventDefault();
+    const $findGameIdInput = document.querySelector("#findGameId");
+    $findGameIdInput.value;
+}
+
+function joinGame() {
+
+}
