9d046f1b10513cd896fc3e4a42f942d4d47a6870
Zjefvm <66432723+Zjefvm@users.noreply.github.com>
2024-03-27 19:35:37 +0100

    Added a check to check if there is input in the create game page

--- src/assets/js/gameCreator.js
+++ src/assets/js/gameCreator.js
@@ -1,6 +1,6 @@
-import { navigate } from './universal.js';
-import { GAMEPREFIX } from './config.js';
-import { saveToStorage } from './data-connector/local-storage-abstractor.js';
+import {navigate} from './universal.js';
+import {GAMEPREFIX} from './config.js';
+import {saveToStorage} from './data-connector/local-storage-abstractor.js';
 import *  as CommunicationAbstractor from './data-connector/api-communication-abstractor.js';
 
 let GAME = {
@@ -14,16 +14,21 @@ let GAME = {
 
 function init() {
     document.querySelector('#back').addEventListener('click', () => navigate('createOrJoin.html'));
-    document.querySelector('form').addEventListener('submit', (e) => createGame(e));
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        checkInput();
+        createGame(e)
+    });
 }
 
 function createGame(e) {
     e.preventDefault();
     const playerName = localStorage.getItem('playerName');
     GAME.playerName = playerName;
-    GAME.gameMode = document.querySelector('input[name="game-mode"]:checked').value
-    GAME.gameName = document.querySelector('#gameName').value;
     GAME.maxPlayers = parseInt(document.querySelector('#max-players').value)
+    if (!document.querySelector('#error').classList.contains('hidden')) {
+        return;
+    }
     postGame(GAME);
     navigate('game.html');
 }
@@ -34,6 +39,26 @@ function postGame(game) {
             saveToStorage("playerToken", response.playerToken);
             saveToStorage("gameId", response.gameId);
         }).catch((error) => console.error(error));
+}
+
+function checkInput() {
+    let checkedRadioButton = document.querySelector('input[name="game-mode"]:checked');
+    let gameNameInput = document.querySelector("#gameName").value.trim();
+    let error = document.querySelector('#error');
+
+    if (checkedRadioButton === null) {
+        error.insertAdjacentHTML('beforeend', '<p>Please select a game mode</p>')
+    } else if(gameNameInput === "") {
+        error.insertAdjacentHTML('beforeend', '<p>Please enter a game name</p>')
+    }
+
+    if (checkedRadioButton === null || gameNameInput === "") {
+        document.querySelector('#error').classList.remove('hidden');
+    } else {
+        document.querySelector('#error').classList.add('hidden');
+        GAME.gameMode = checkedRadioButton.value;
+        GAME.gameName = gameNameInput;
+    }
 }
 
 init();
