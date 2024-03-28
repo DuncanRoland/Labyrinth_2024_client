c2c09249e8393ce9ee559512ffc128f775ba1b46
grump <happyalex24563@gmail.com>
2024-03-28 15:40:43 +0100

    Fix post games with updated API

--- src/assets/js/gameCreator.js
+++ src/assets/js/gameCreator.js
@@ -1,6 +1,6 @@
-import {navigate} from './universal.js';
-import {GAMEPREFIX} from './config.js';
-import {saveToStorage} from './data-connector/local-storage-abstractor.js';
+import { navigate } from './universal.js';
+import { GAMEPREFIX } from './config.js';
+import { saveToStorage } from './data-connector/local-storage-abstractor.js';
 import *  as CommunicationAbstractor from './data-connector/api-communication-abstractor.js';
 
 let GAME = {
@@ -10,13 +10,13 @@ let GAME = {
     gameName: 'testGame',
     minPlayers: 2,
     maxPlayers: 4,
+    numberOfTreasuresPerPlayer: 3,
 };
 
 function init() {
     document.querySelector('#back').addEventListener('click', () => navigate('createOrJoin.html'));
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault();
-        checkInput();
         createGame(e)
     });
 }
@@ -24,18 +24,22 @@ function init() {
 function createGame(e) {
     e.preventDefault();
     const playerName = localStorage.getItem('playerName');
-    GAME.playerName = playerName;
-    GAME.maxPlayers = parseInt(document.querySelector('#max-players').value)
+    const game = GAME;
+    game.playerName = playerName;
+    game.gameName = document.querySelector('#gameName').value;
+    game.maxPlayers = parseInt(document.querySelector('#max-players').value)
     if (!document.querySelector('#error').classList.contains('hidden')) {
         return;
     }
-    postGame(GAME);
-    navigate('game.html');
+    postGame(game);
+    //navigate('game.html');
 }
 
 function postGame(game) {
+    console.log(game)
     CommunicationAbstractor.fetchFromServer('/games', 'POST', game)
         .then(response => {
+            console.log(response)
             saveToStorage("playerToken", response.playerToken);
             saveToStorage("gameId", response.gameId);
         }).catch((error) => console.error(error));
@@ -48,7 +52,7 @@ function checkInput() {
 
     if (checkedRadioButton === null) {
         error.insertAdjacentHTML('beforeend', '<p>Please select a game mode</p>')
-    } else if(gameNameInput === "") {
+    } else if (gameNameInput === "") {
         error.insertAdjacentHTML('beforeend', '<p>Please enter a game name</p>')
     }
 
