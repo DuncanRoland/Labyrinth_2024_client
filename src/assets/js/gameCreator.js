0f3f4f41c0cad892d69572207e6aa1c8c330efd3
grump <happyalex24563@gmail.com>
2024-03-29 09:37:07 +0100

    refactor errorhandling for form

--- src/assets/js/gameCreator.js
+++ src/assets/js/gameCreator.js
@@ -12,13 +12,14 @@ let GAME = {
     maxPlayers: 4,
     numberOfTreasuresPerPlayer: 3,
 };
+const maxCharacters = 15;
 
 function init() {
     document.querySelector('#back').addEventListener('click', () => navigate('createOrJoin.html'));
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault();
-        checkInput();
-        createGame(e)
+        checkForm();
+        createGame(e);
     });
 }
 
@@ -46,30 +47,28 @@ function postGame(game) {
         }).catch((error) => console.error(error));
 }
 
-function checkInput() {
-    let checkedRadioButton = document.querySelector('input[name="game-mode"]:checked');
-    let gameNameInput = document.querySelector("#gameName").value.trim();
-    let error = document.querySelector('#error');
-    let maxCharacters = 15;
+function checkForm() {
+    const checkedRadioButton = document.querySelector('input[name="game-mode"]:checked');
+    const gameNameInput = document.querySelector("#gameName").value.trim();
+    const error = document.querySelector('#error');
+    error.innerHTML = "";
 
-    if (checkedRadioButton === null) {
+    if(checkedRadioButton === null){
         error.insertAdjacentHTML('beforeend', '<p>Please select a game mode</p>')
-    } else if (gameNameInput === "") {
+    } else if(gameNameInput === ""){
         error.insertAdjacentHTML('beforeend', '<p>Please enter a game name</p>')
+    } else if(gameNameInput.length > maxCharacters){
+        error.insertAdjacentHTML('beforeend', `<p>Game name cannot exceed ${maxCharacters} characters</p>`)
     }
 
-    if (checkedRadioButton === null || gameNameInput === "") {
+    if(checkedRadioButton === null || gameNameInput === "" || gameNameInput.length > maxCharacters){
         document.querySelector('#error').classList.remove('hidden');
     } else {
         document.querySelector('#error').classList.add('hidden');
         GAME.gameMode = checkedRadioButton.value;
         GAME.gameName = gameNameInput;
     }
-
-    if (gameNameInput.length > maxCharacters) {
-        error.insertAdjacentHTML('beforeend', '<p>Game name cannot exceed ' + maxCharacters + ' characters</p>')
-        document.querySelector('#error').classList.remove('hidden');
-    }
+   
 }
 
 init();
