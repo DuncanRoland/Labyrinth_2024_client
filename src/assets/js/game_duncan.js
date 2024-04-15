c5aeaf5dd7da33cc56a28e7ea87759abfe29d11d
Duncan Roland <duncan.roland@student.howest.be>
2024-04-15 19:23:23 +0200

    added my code to latest merge

--- src/assets/js/game_duncan.js
+++ src/assets/js/game_duncan.js
@@ -3,10 +3,9 @@ import * as ErrorHandler from "./data-connector/error-handler.js";
 import { loadFromStorage } from "./data-connector/local-storage-abstractor.js";
 import { navigate } from "./universal.js";
 import { TIMEOUTDELAY } from "./config.js";
-
-const GAMEMAXTREASURES = await getDescription(loadFromStorage("gameId")).then(response => response.description.numberOfTreasuresPerPlayer).catch(ErrorHandler.handleError);
-const PLAYERNAME = localStorage.getItem("playerName");
-const GAMEID = loadFromStorage("gameId");
+const GAMEMAXTREASURES = await getActiveGameDetails(loadFromStorage('gameId')).then(response => response.description.numberOfTreasuresPerPlayer).catch(ErrorHandler.handleError);
+const PLAYERNAME = localStorage.getItem('playerName');
+const GAMEID = loadFromStorage('gameId');
 
 const slideIndicators = `
         <div class="slide-indicator slide-indicator-top-left"></div>
@@ -23,69 +22,74 @@ const slideIndicators = `
         <div class="slide-indicator slide-indicator-right-bottom"></div>
     `;
 
+let shove = {
+    "destination": {
+        "row": 1,
+        "col": 0
+    },
+    "tile": {
+        "walls": [
+            true,
+            false,
+            true,
+            false
+        ],
+        "treasure": null
+    }
+}
+
+let move ={
+    "destination": {
+        "row": 0,
+        "col": 0
+    }
+}
+
 init();
 
 
 function init() {
     generateBoard();
-    createEventListeners();
+    createInitialEventListeners();
     createTreasureObjectives(GAMEMAXTREASURES);
-    getPlayers();
-    displayObtainedTreasures();
-    displayMovableTile();
+    polling();
 }
 
 async function generateBoard() {
-    const board = document.querySelector("#board");
-    const boardBackground = document.querySelector("#boardBackground");
+    const board = document.querySelector('#board');
+    const boardBackground = document.querySelector('#background');
     const maze = await getMaze();
-    console.log(maze);
-    for (const row of maze.maze) {
-        console.log(row);
-        for (const cell of row) {
-            const square = document.createElement("div");
-            square.classList.add("square");
+    for (const [rowIndex, row] of maze.maze.entries()) {
+        for (const [cellIndex, cell] of row.entries()) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.dataset.coordinates =  `${rowIndex},${cellIndex}`;
             generateRandomTilesImg(square, cell.walls);
             addTreasuresToBoard(square, cell);
             board.appendChild(square);
         }
     }
-
-    /*
-    for (let columns = 0; columns < maxColumns; columns++) {
-        const column = document.createElement('div');
-        column.classList.add('column');
-        board.appendChild(column);
-        for (let rows = 0; rows < maxRows; rows++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            generateRandomTilesImg(square);
-            square.setAttribute('data-target', `${columns},${rows}`);
-            column.appendChild(square);
-        }
-    }*/
-    boardBackground.insertAdjacentHTML("beforeend", slideIndicators);
+    boardBackground.insertAdjacentHTML('beforeend', slideIndicators);
 }
 
 function generateRandomTilesImg(element, walls) {
     const wallTile = getWallImageId(walls);
-    element.insertAdjacentHTML("beforeend", `<img src="assets/media/tiles/${wallTile}.png">`);
+    element.insertAdjacentHTML('beforeend', `<img src="assets/media/tiles/${wallTile}.png">`);
 
 }
 
-function createEventListeners() {
-    const button = document.querySelector("button");
-    const allBoardPieces = document.querySelectorAll(".square img");
-    button.addEventListener("click", () => leaveGame());
-    allBoardPieces.forEach(boardPiece => boardPiece.addEventListener("click", (e) => getBoardPiece(e)));
+function createInitialEventListeners() {
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => leaveGame());
 }
 
 function getBoardPiece(e) {
     e.preventDefault();
-    console.log(e.currentTarget.parentElement.dataset.target);
+    console.log(e.currentTarget.dataset.coordinates);
+    shoveTile(e.currentTarget.dataset.coordinates)
 }
 
-async function createTreasureObjectives(maxObjectives = 3) {
+async function createTreasureObjectives(maxObjectives = 5) {
     const treasures = await fetchTreasures().catch(ErrorHandler.handleError);
     const objectives = getObjectiveList(treasures, maxObjectives);
     displayPlayerObjectives(objectives);
@@ -111,6 +115,12 @@ function getRandomObjective(treasures) {
     return treasures.treasures[randomIndex];
 }
 
+/*function createDiv(elementName, inner, container) {
+    const element = document.createElement(elementName);
+    element.innerHTML = inner;
+    container.appendChild(element);
+}*/
+
 function displayPlayerObjectives(objectives) {
     const $treasureList = document.querySelector("#treasureList");
     $treasureList.innerHTML = "";
@@ -124,62 +134,36 @@ function displayPlayerObjectives(objectives) {
     });
 }
 
-// needs further implementation after board objectives work
-function displayObtainedTreasures() {
-    const $obtainedTreasures = document.querySelector("#obtainedTreasures");
-    $obtainedTreasures.innerHTML = "";
-
-    //static placeholder for now
-    for (let i = 0; i < 3 ; i++) {
-        const li = `<li>
-                    <img src="assets/media/treasures_cards/Bag_of_Gold_Coins.JPG" alt="Bag of Gold Coins">
-                </li>`;
-        $obtainedTreasures.insertAdjacentHTML("beforeend", li);
-    }
-
-}
-
-function displayMovableTile() {
-    const $movableTile = document.querySelector("#movableTile");
-    $movableTile.innerHTML = "";
-
-    const img = `<img src="../media/scanned_tiles/Straight_tile.JPG" alt="Bag of Gold Coins">`;
-    $movableTile.insertAdjacentHTML("beforeend", img);
-}
-
-/*function createDiv(elementName, inner, container) {
-    const element = document.createElement(elementName);
-    element.innerHTML = inner;
-    container.appendChild(element);
-}*/
-
-async function getPlayers() {
-    await getDescription(GAMEID)
+async function polling() {
+    await getActiveGameDetails(GAMEID)
         .then(response => {
+            //setTimeout(refreshBoard, TIMEOUTDELAY);
+            boardEventListeners();
+            showTurn(response);
+            DisplayObtainedTreasures();
             displayPlayers(response.description.players);
-            setTimeout(getPlayers, TIMEOUTDELAY);
+            setTimeout(polling, TIMEOUTDELAY);
             console.log(`Lobby: ${response.description.players.length}/${response.description.maxPlayers}`);
         });
+    const a = await getReachableLocations();
 }
 
 function displayPlayers(players) {
     const playerList = document.querySelector("#playerList");
     playerList.innerHTML = "";
-    players.forEach(player => {
-        playerList.insertAdjacentHTML("beforeend", `<li>${player}</li>`);
-    });
+    players.forEach(player => { playerList.insertAdjacentHTML('beforeend', `<li>${player}</li>`) });
 }
 
-async function getDescription(gameId) {
-    return await getAPIResponse(gameId, "description=true", "GET");
+async function getActiveGameDetails(gameId) {
+    return await getAPIResponse(gameId, 'description=true&players=true&spareTile=true', 'GET');
 }
 
 
 async function leaveGame() {
-    return await CommunicationAbstractor.fetchFromServer(`/games/${GAMEID}/players/${PLAYERNAME}`, "DELETE")
+    return await CommunicationAbstractor.fetchFromServer(`/games/${GAMEID}/players/${PLAYERNAME}`, 'DELETE')
         .then(response => {
             console.log(response);
-            navigate("createOrJoin.html");
+            navigate('createOrJoin.html');
         }).catch(ErrorHandler.handleError);
 }
 
@@ -188,7 +172,7 @@ async function getAPIResponse(path, parameters, method) {
 }
 
 async function getMaze() {
-    return await getAPIResponse(GAMEID, "description=false&maze=true", "GET");
+    return await getAPIResponse(GAMEID, 'description=false&maze=true', 'GET');
 }
 
 function getWallImageId(walls) {
@@ -202,7 +186,7 @@ function getWallImageId(walls) {
         "false,false,false,true": 7, // left side T
         "false,true,false,false": 9, // right side T
         "false,false,true,false": 1, // upside down T
-        "true,false,false,false": 8 // T
+        "true,false,false,false": 8, // T
     };
     return wallConfigurations[walls.toString()];
 }
@@ -210,7 +194,71 @@ function getWallImageId(walls) {
 function addTreasuresToBoard(square, cell) {
     if (cell.treasure) {
         square.dataset.treasure = cell.treasure;
-        const treasure = cell.treasure.replace(" ", "_");
-        square.insertAdjacentHTML("beforeend", `<img src="assets/media/scanned_tiles/${treasure}_tile.jpg" class="treasure">`);
+        const treasure = cell.treasure.replaceAll(' ', '_');
+        square.insertAdjacentHTML('beforeend', `<img src="assets/media/treasure_cutouts/${treasure}.png" class="treasure">`);
+    }
+}
+
+function refreshBoard() {
+    const board = document.querySelector('#board');
+    const boardBackground = document.querySelector('#background');
+    board.innerHTML = "";
+    boardBackground.innerHTML = "";
+    generateBoard();
+}
+
+function showTurn(data) {
+    const player = document.querySelector('#turnOrder');
+    if (data.description.currentShovePlayer !== null) {
+        player.innerHTML = `Current shove turn: ${data.description.currentShovePlayer}`;
+    } else if (data.description.currentMovePlayer !== null){
+        player.innerHTML = `Current move turn: ${data.description.currentMovePlayer}`;
+    }
+}
+
+async function shoveTile(coordinates) {
+    const gameDetails = await getActiveGameDetails(GAMEID);
+    shove.destination.row = parseInt(coordinates[0]);
+    shove.destination.col = parseInt(coordinates[2]);
+    shove.tile = gameDetails.spareTile;
+    return await CommunicationAbstractor.fetchFromServer(`/games/${GAMEID}/maze`, 'PATCH', shove).catch(ErrorHandler.handleError);
+}
+
+async function movePlayer(coordinates) {
+    move.destination.row = coordinates[0];
+    move.destination.col = coordinates[2];
+    return await CommunicationAbstractor.fetchFromServer(`/games/${GAMEID}/players/${PLAYERNAME}`, 'PATCH', move).catch(ErrorHandler.handleError);
+}
+
+async function getReachableLocations(){
+    const playerDetails = await getPlayerDetails();
+    const playerRow = playerDetails.player.location.row;
+    const playerCol = playerDetails.player.location.col;
+    return await CommunicationAbstractor.fetchFromServer(`/games/${GAMEID}/maze/locations/${playerRow}/${playerCol}`);
+}
+
+async function getPlayerDetails(){
+    return await CommunicationAbstractor.fetchFromServer(`/games/${GAMEID}/players/${PLAYERNAME}`);
+}
+
+function boardEventListeners(){
+    const allBoardPieces = document.querySelectorAll('.square');
+    allBoardPieces.forEach(boardPiece => {
+            boardPiece.addEventListener('click', (e) => getBoardPiece(e));
+        }
+    );
+}
+
+async function DisplayObtainedTreasures(){
+    const $obtainedTreasures = document.querySelector("#obtainedTreasures");
+    $obtainedTreasures.innerHTML = "";
+    const playerDetails = await getPlayerDetails();
+    // playerDetails.player.foundTreasures.forEach(treasure => $obtainedTreasure.insertAdjacentHTML("beforeend", `<li>${treasure}</li>`))
+    //static placeholder for now
+    for (let i = 0; i < 5 ; i++) {
+        const li = `<li>
+                    <img src="assets/media/treasures_cards/Bag_of_Gold_Coins.JPG" alt="Bag of Gold Coins">
+                </li>`;
+        $obtainedTreasures.insertAdjacentHTML("beforeend", li);
     }
 }
