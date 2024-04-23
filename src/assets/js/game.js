87952adc9581f953b1ea144a472135a886584c00
grump <happyalex24563@gmail.com>
2024-04-23 12:40:11 +0200

    Give notification (needed design upgrade) + autoload gamedetails

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -30,14 +30,8 @@ init();
 async function init() {
     displayWhoYouAre();
     boardEventListeners();
-    await generateBoard();
-    createInitialEventListeners();
-    createTreasureObjectives(GAMEMAXTREASURES);
     polling();
-    getAndDisplaySpareTile();
-    rotateSpareTileButton();
-    refreshBoard();
-
+    createInitialEventListeners();
 }
 
 async function generateBoard() {
@@ -147,17 +141,21 @@ async function polling() {
         await deleteGame();
         return;
     }
-
-    setTimeout(refreshBoard, TIMEOUTDELAY);
-
-    getObjectiveIndex();
-    showTurn(gameDetails);
-    displayObtainedTreasures();
-    displayPlayerList(gameDetails.description.players);
-
-    setTimeout(polling, TIMEOUTDELAY);
-    console.log(`Lobby: ${gameDetails.description.players.length}/${gameDetails.description.maxPlayers}`);
-
+    if (gameDetails.description.started === false) {
+        waitingForPlayers();
+        setTimeout(polling, TIMEOUTDELAY);
+    } else{
+        removePopUp();
+        refreshBoard();
+        getObjectiveIndex();
+        showTurn(gameDetails);
+        displayObtainedTreasures();
+        displayPlayerList(gameDetails.description.players);
+    
+        setTimeout(polling, TIMEOUTDELAY);
+        console.log(`Lobby: ${gameDetails.description.players.length}/${gameDetails.description.maxPlayers}`);
+    }
+    
 }
 
 async function deleteGame() {
@@ -503,4 +501,21 @@ async function getPlayerNameAtCoordinates(row, col) {
 function displayWhoYouAre() {
     const $playerName = document.querySelector("#joinedPlayer");
     $playerName.innerHTML = `${PLAYERNAME}`;
+}
+
+function waitingForPlayers() {
+    const $waitingForPlayers = document.querySelector("#popUp");
+    $waitingForPlayers.innerHTML = "";
+    $waitingForPlayers.insertAdjacentHTML("beforeend", `
+        <div id="popUpContent">
+        <h2 id="popUpTitle">Game is not ready</h2>
+        <p id="popUpText">Waiting for players</p
+        </div>`);
+}
+
+function removePopUp(){
+    const $popUp = document.querySelector("#popUp");
+    if ($popUp.innerHTML !== ""){
+        $popUp.innerHTML = "";
+    }
 }
