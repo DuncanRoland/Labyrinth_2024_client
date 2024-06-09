a3090b9e059d14f973887106942092c5e527b503
Duncan Roland <duncan.roland@student.howest.be>
2024-06-09 03:50:13 +0200

    changed url

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -5,12 +5,12 @@ const TIMEOUTDELAY = 2500;
 
 const ERRORHANDLERSELECTOR = ".errormessages p";
 
-const LOCALSERVER = `http://localhost:8080`;
+const LOCALSERVER = `http://localhost:8000`;
 const DEPLOYEDSERVER = `https://project-1.ti.howest.be/2023-2024/labyrinth/api`;
 const GROUPDEPLOYEDSERVER = `https://project-1.ti.howest.be/2023-2024/${LANG}/group-${GROUPNUMBER}/api`;
 
 function getAPIUrl() {
-  return DEPLOYEDSERVER;
+  return LOCALSERVER;
 }
 
 export { getAPIUrl, GROUPNUMBER, GAMEPREFIX, ERRORHANDLERSELECTOR, TIMEOUTDELAY };
